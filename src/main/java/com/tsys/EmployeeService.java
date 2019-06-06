c8c4ead298f36ebc170fb9c754b338aef816e159
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2019-06-06 15:21:30 +0530

    Added employee

--- src/main/java/com/tsys/EmployeeService.java
+++ src/main/java/com/tsys/EmployeeService.java
@@ -1,22 +1,34 @@
 package com.tsys;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(path = "employee")
 public class EmployeeService {
-	@RequestMapping(method = RequestMethod.POST)
-	public void create() {
-		System.out.println("EMployee Created");
+	@RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+	public void create(@RequestBody Employee employee) {
+		System.out.println("Employee Created");
 	}
+
 	@RequestMapping(method = RequestMethod.GET)
-	public void read() {
+	@ResponseBody
+	public Employee read() {
 		System.out.println("Employeee Read");
+		Employee employee = new Employee();
+		employee.setId(1L);
+		employee.setName("John Doe");
+		return employee;
 	}
+
 	@RequestMapping(method = RequestMethod.PUT)
 	public void update() {
 		System.out.println("Employee updated");
 	}
+
 	@RequestMapping(method = RequestMethod.DELETE)
 	public void delete() {
 		System.out.println("Employee Deleted");
